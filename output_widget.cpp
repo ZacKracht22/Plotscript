de6a8c9ba2b2bb07413ffda7291370072536cf93
Zac Kracht <kracht22@vt.edu>
2018-10-25 11:28:53 -0400

    Error checking in point and line and text

--- output_widget.cpp
+++ output_widget.cpp
@@ -40,13 +40,14 @@ void OutputWidget::outputPoint(Expression& exp) {
 	qreal height = width;
 
 	if (width < 0) {
-		//error
+		outputExpression(QString::fromStdString("Error: point size cannot be negative"));
 	}
-
-	QGraphicsEllipseItem* point = new QGraphicsEllipseItem(x,y,width,height);
-	QBrush brush(Qt::SolidPattern);
-	point->setBrush(brush);
-	qgs->addItem(point);
+	else {
+		QGraphicsEllipseItem* point = new QGraphicsEllipseItem(x, y, width, height);
+		QBrush brush(Qt::SolidPattern);
+		point->setBrush(brush);
+		qgs->addItem(point);
+	}
 
 	/*qDebug() << "I am point";
 	qDebug() << "X: " << x;
@@ -58,24 +59,24 @@ void OutputWidget::outputPoint(Expression& exp) {
 void OutputWidget::outputLine(Expression& exp) {
 	clear();
 
-	qreal x1 = exp.getTail().at(0).getTail().at(0).getHead().asNumber();
-	qreal x2 = exp.getTail().at(1).getTail().at(0).getHead().asNumber();
-	qreal y1 = exp.getTail().at(0).getTail().at(1).getHead().asNumber();
-	qreal y2 = exp.getTail().at(1).getTail().at(1).getHead().asNumber();
-
-	QGraphicsLineItem* line = new QGraphicsLineItem(x1, y1, x2, y2);
-
-	QPen pen(Qt::SolidLine);
 	int thickness = exp.getProperty("\"thickness\"").getHead().asNumber();
 
 	if (thickness < 0) {
-		//error
+		outputExpression(QString::fromStdString("Error: line thickness cannot be negative"));
 	}
+	else {
+		qreal x1 = exp.getTail().at(0).getTail().at(0).getHead().asNumber();
+		qreal x2 = exp.getTail().at(1).getTail().at(0).getHead().asNumber();
+		qreal y1 = exp.getTail().at(0).getTail().at(1).getHead().asNumber();
+		qreal y2 = exp.getTail().at(1).getTail().at(1).getHead().asNumber();
 
-	pen.setWidth(thickness);
-	line->setPen(pen);
-	qgs->addItem(line);
+		QGraphicsLineItem* line = new QGraphicsLineItem(x1, y1, x2, y2);
 
+		QPen pen(Qt::SolidLine);
+		pen.setWidth(thickness);
+		line->setPen(pen);
+		qgs->addItem(line);
+	}
 	/*qDebug() << "I am line";
 	qDebug() << "X1: " << x1;
 	qDebug() << "Y1: " << y1;
@@ -88,13 +89,20 @@ void OutputWidget::outputLine(Expression& exp) {
 void OutputWidget::outputText(Expression& exp) {
 	clear();
 
-	std::string temp = exp.getHead().asString();
-	temp.erase(0, 1);
-	temp.erase(temp.length() - 1, 1);
-	QString text = QString::fromStdString(temp);
-	QGraphicsTextItem * qgti = new QGraphicsTextItem(text);
+	Atom shouldBeList = exp.getProperty("\"position\"").getHead();
+	std::vector<Expression> point = exp.getProperty("\"position\"").getTail();
 
-	qgs->addItem(qgti);
+	if (shouldBeList != Atom("list") || point.size() != 2) {
+		outputExpression(QString::fromStdString("Error: position must be a point"));
+	}
+	else {
+		std::string temp = exp.getHead().asString();
+		temp.erase(0, 1);
+		temp.erase(temp.length() - 1, 1);
+		QString text = QString::fromStdString(temp);
+		QGraphicsTextItem * qgti = new QGraphicsTextItem(text);
+		qgs->addItem(qgti);
+	}
 
 	//qDebug() << "I am text";
 }
